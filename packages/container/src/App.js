1296bbb8db6f7750a9b565c380058eea0e771837
stephen grider <ste.grider@gmail.com>
2020-11-06 15:47:45 -0700

    updated text

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from './components/MarketingApp';
 export default () => {
   return (
     <div>
-      <h1>Hi there!</h1>
+      <h1>Hi there!!!!</h1>
       <hr />
       <MarketingApp />
     </div>
