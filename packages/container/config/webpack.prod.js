6fddb8e611800b33e9cebca554610e621dfdb41f
stephen grider <ste.grider@gmail.com>
2020-11-06 15:29:33 -0700

    added publicpath

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -9,6 +9,7 @@ const prodConfig = {
   mode: 'production',
   output: {
     filename: '[name].[contenthash].js',
+    publicPath: '/container/latest/',
   },
   plugins: [
     new ModuleFederationPlugin({
