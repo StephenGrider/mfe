532715b921aee9887f5e2fb001c738f1aa91e442
stephen grider <ste.grider@gmail.com>
2020-11-07 09:20:59 -0700

    Added marketing deployment

--- packages/marketing/src/components/Landing.js
+++ packages/marketing/src/components/Landing.js
@@ -128,7 +128,7 @@ export default function Album() {
                     </Typography>
                     <Typography>
                       This is a media card. You can use this section to describe
-                      the content.
+                      the content!
                     </Typography>
                   </CardContent>
                   <CardActions>
