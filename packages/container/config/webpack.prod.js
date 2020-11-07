c51dd1b1b2f1e1f126ec30bfb1fe1b28cec25dee
stephen grider <ste.grider@gmail.com>
2020-11-07 09:18:17 -0700

    added marketing deployment

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -15,7 +15,7 @@ const prodConfig = {
     new ModuleFederationPlugin({
       name: 'container',
       remotes: {
-        marketing: `marketing@${domain}/marketing/remoteEntry.js`,
+        marketing: `marketing@${domain}/marketing/latest/remoteEntry.js`,
       },
       shared: packageJson.dependencies,
     }),
